bc279f28cbbd2d8095eba101b4d6712aa4ba6823
meng.wm <meng.wm@alibaba-inc.com>
2020-06-27 22:06:53 +0800

    add audio utils

---
+++ packages/web-util-kit/src/audio.ts
@@ -0,0 +1,53 @@
+
+const MAX_CACHED_COUNT = 10;
+
+
+export class AudioBufferLoader {
+
+  cached: any = {};
+
+  constructor(private context: AudioContext) { }
+
+  async loadBuffer(url: string) {
+    // Load buffer asynchronously
+    if (this.cached[url]) {
+      return this.cached[url];
+    }
+
+    const request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+    
+
+    return new Promise((resolve, reject) => {
+      request.onload = () => {
+        // Asynchronously decode the audio file data in request.response
+        this.context.decodeAudioData(
+          request.response,
+          (buffer) => {
+            if (!buffer) {
+              reject(new Error('error decoding file data: ' + url));
+              return;
+            }
+            // TODO use LRU cached
+            this.cached[url] = buffer;
+            resolve(buffer);
+
+          },
+          function(error) {
+            reject(new Error('decodeAudioData error' + error));
+          }
+        );
+      }
+
+      request.onerror = function() {
+        reject(new Error('BufferLoader: XHR error'));
+      }
+
+      request.send();
+    });    
+  }
+
+
+}
+
--- packages/web-util-kit/src/index.ts
+++ packages/web-util-kit/src/index.ts
@@ -3,4 +3,5 @@ export * from './image';
 export * from './dom';
 export * from './canvas';
 export * from './pHash';
-export * from './image-layout';
+export * from './align';
+export * from './audio';
