53265d1d77ea5d6e26408f1b0d0c21f669585df5
Meng Wang <wangmeng_hb@163.com>
2020-12-04 16:23:55 +0800

    fix comments

--- packages/gl-image/src/gl-utils.ts
+++ packages/gl-image/src/gl-utils.ts
@@ -82,16 +82,12 @@ export function initFilter(gl: WebGLRenderingContext, program: WebGLProgram) {
 }
 
 function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
-  // 创建着色器对象
   let shader = gl.createShader(type);
   if (shader == null) {
     throw new Error('faided to create webgl shader!');
   }
-  // 设置着色器源代码
   gl.shaderSource(shader, source);
-  // 编译着色器
   gl.compileShader(shader);
-  // 检查着色器的编译状态
   let compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
   if (!compiled) {
       let error = gl.getShaderInfoLog(shader);
@@ -103,27 +99,22 @@ function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
 }
 
 function createProgram(gl: WebGLRenderingContext, vshader: string, fshader: string) {
-  // 创建着色器对象
   let vertexShader = loadShader(gl, gl.VERTEX_SHADER, vshader);
   let fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fshader);
   if (!vertexShader || !fragmentShader) {
       return null;
   }
-  // 创建程序对象
   let program = gl.createProgram();
   if (!program) {
       return null;
   }
-  // 为程序对象分配顶点着色器和片元着色器
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
-  // 连接着色器
   gl.linkProgram(program);
-  // 检查连接
   let linked = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (!linked) {
       let error = gl.getProgramInfoLog(program);
-      console.log('无法连接程序对象: ' + error);
+      console.error('can NOT link gl program: ' + error);
       gl.deleteProgram(program);
       gl.deleteShader(fragmentShader);
       gl.deleteShader(vertexShader);
@@ -140,7 +131,7 @@ export function initShaders(gl: WebGLRenderingContext, shaderObj: any) {
   
   const result = createProgram(gl, shaderObj.vshader, shaderObj.fshader);
   if (!result) {
-      console.log('无法创建程序对象');
+      console.log('can NOT create gl program');
       return false;
   }
   const { program, vertexShader, fragmentShader } = result;
