3c81246628b8d3f5e8afd8a5e1065f6b20fbcaaa
Meng Wang <wangmeng_hb@163.com>
2020-12-11 15:28:49 +0800

    test

--- docs/assets/test.js
+++ docs/assets/test.js
@@ -1,7 +1,9 @@
 
 document.addEventListener('load', () => {
 
-    const root = document.getElementById('root');
+    const root = document.getElementById('test_root');
+
+    console.log('root', root);
 
     if (root) {
         alert('root exist');
